1f2dc386d2ea872396d13883f021e8e44926c526
Henrique Pinho <henrique.de.nf@gmail.com>
2020-07-17 11:22:39 -0300

    js pegando dados do form

---
+++ JS/Operacao.js
@@ -0,0 +1,9 @@
+class Operacao {
+    
+    constructor(descricao, venc, auto, valor) {
+        this.descricao = descricao;
+        this.venc = venc;
+        this.auto = auto;
+        this.valor = valor;
+    }
+}
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function alerta() {
-    alert("passo");
+function adicionar() {
+    var descricao = document.getElementById('descricao').value;
+    var vencimento = parseInt(document.getElementById('venc').value);
+    var automatico = document.getElementById('auto').checked;
+    var valor = parseFloat(document.getElementById('valor').value);
+
+    console.log(descricao, vencimento, automatico, valor);
+
 }
