6d428636647cbd3d88c66685cc945d2af8090ef0
Henrique Pinho <henrique.de.nf@gmail.com>
2020-07-23 12:41:46 -0300

    functions geraLinha, contador and automatico

--- js/index.js
+++ js/index.js
@@ -1,19 +1,35 @@
 let transacao = new Formulario(1, true, 'Pagamento', 15, 150.50);
+let i = 0;
 
 console.log(transacao);
 
-function geraTabela(conteudo) {
+function geraLinha(auto, descricao, vencimento, valor) {
 
     let tabela = document.querySelector('#tabela-principal');
 
-    tr = document.createElement('tr');
-    td = document.createElement('td');
-    td.textContent = conteudo;
-
-    tr.appendChild(td);
-    tabela.appendChild(tr);
+    let linhaTabela = `
+        <tr>
+            <td class="tr-numero">${contador()}</td>
+            <td class="tr-pago"><input type="checkbox" name="pago"></td>
+            <td class="tr-auto">${automatico(auto)}</td>
+            <td class="tr-descricao">${descricao}</td>
+            <td class="tr-vencimento">${vencimento}</td>
+            <td class="tr-valor">${valor}</td>
+        </tr>
+    `
+    tabela.insertAdjacentHTML('beforeend', linhaTabela);
 
     console.log(tabela);
 }
 
-//tentar template string e insertAdjacentHTML
+function contador() {
+
+    i += 1;
+    return i;
+}
+
+function automatico(valorBool) {
+    
+    if(valorBool) return '<input type="checkbox" name="auto" checked>'
+    else return '<input type="checkbox" name="auto"></input>'
+}
